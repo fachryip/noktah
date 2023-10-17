e1774bdda0474b2ed9f2af4d2e5a73ae210be4f4
Fachry Ichsan Putra <fachry.ichsan.putra@polytron.co.id>
2023-10-17 10:15:16 +0700

    temp goal

---
+++ Assets/Code/Core/Behaviour/Transform/AlwaysRotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Noktah
+{
+    public class AlwaysRotate : MonoBehaviour
+    {
+        [SerializeField] private Vector3 Direction;
+
+        private void FixedUpdate()
+        {
+            transform.Rotate(Direction);
+        }
+    }
+}
